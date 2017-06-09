dc7579efaf79fcee0ab4007d9992ca28d8b6fb9a
youlangu <youlangu2015@126.com>
2017-06-09 11:24:51 +0800

    use koa-router to manage the router

--- app.js
+++ app.js
@@ -1,60 +1,71 @@
 `use strict`;
 
 const Koa = require("koa");
+const Router = require("koa-router");
 
 const app = new Koa();
+const router = new Router();
 
-//启动端口
 const PORT = 3000;
 
 //采用列表模拟数据库
 var todolist = {
-  limit: 5,
-  offset:0,
-  total:3,  
-  tasks: [
-    {
-      id: 1,
-      title: "learn koa",
-      done: false
-    },
-    {
-      id: 2,
-      title: "design project",
-      done: false
-    },
-    {
-      id: 3,
-      title: "todo project",
-      done: false
-    }
-  ]
+    tasks: [
+      {
+        id: 1,
+        title: "learn koa",
+        done: false
+      },
+      {
+        id: 2,
+        title: "design project",
+        done: false
+      },
+      {
+        id: 3,
+        title: "todo project",
+        done: false
+      }
+    ]
 };
 
 //log middleware
 app.use(async (ctx, next) => {
-  console.log(`URL : ${ctx.request.method} ${ctx.request.url}...`);
+  console.log(`URL : ${ctx.request.method} ${ctx.request.url}`);
   await next();
 });
 
-app.use(async (ctx, next) => {
-  if (ctx.request.path === "/") {
-    ctx.response.type = "text/html";
-    ctx.response.body = "<h1>Hello koa2!</h1>";
-  } else {
-    await next();
-  }
+// get /
+router.get('/', async (ctx, next) => {
+    ctx.response.body = '<h1>Hello koa2!</h1>';
 });
 
-app.use(async (ctx, next) => {
-  if (ctx.request.path === "/api/v1/tasks" && ctx.request.method === "GET") {
-    todolist.total = todolist.tasks.length;
-    ctx.response.type = "application/json";
-    ctx.response.body = JSON.stringify(todolist);
-  } else {
-    await next();
-  }
+//get tasks router
+router.get('/api/v1/tasks', async (ctx, next) => {
+  ctx.response.type = 'application/json';
+  ctx.response.body = JSON.stringify(todolist);
 });
+
+//get tasks/task_id router
+router.get('/api/v1/tasks/:task_id', async (ctx, next) => {
+    var task_id = ctx.params.task_id;
+    var result = '{"code":1001,"msg":"task_not_found"}';
+    var status = 404;
+
+    ctx.response.type = 'application/json';
+    for(var i in todolist.tasks){
+        if(task_id === todolist.tasks[i].id.toString()){
+            result = JSON.stringify(todolist.tasks[i]);
+            status = 200;
+            break;           
+        }
+    }
+    ctx.response.body = result; 
+    ctx.response.status = status;    
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(PORT);
 
